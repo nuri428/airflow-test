182fd63a81f15af4e7f7d1fbdfd3183d058065cd
nuri428 <greennuri@gmail.com>
2023-09-01 16:55:08 +0900

    python task test

--- dags/dags_python_templates.py
+++ dags/dags_python_templates.py
@@ -14,22 +14,14 @@ with DAG(
     dag_id="dags_python_templates",
     schedule="0 0 * * *",#crontab 
     start_date=pendulum.datetime(2021, 8, 31, tz="Asia/Seoul"),
-    catchup=True,
+    catchup=False,
     # dagrun_timeout=datetime.timedelta(minutes=60), timeout 
-    tags=["example", "example2"],
-    params={"example_key": "example_value"}, # task에 공통적으로 넘길 파라메터 
-) as dag:    
-    
+    tags=["example", "inflearn"]    
+) as dag:        
     def python_function1(start_date, end_date, **kwargs):
         print(start_date)
         print(end_date)
-
-    python_t1 = PythonOperator(
-        task_id="python_t1",
-        python_callable=python_function1,
-        op_kwargs={'start_date':"{{data_interval_start | ds}}", "end_date":"{{data_interval_end | ds}}"}
-    )
-
+    
     @task(task_id='pyton_t2')
     def python_function2(**kwargs):
         print(kwargs)
@@ -39,5 +31,20 @@ with DAG(
         print('data_interval_start:'+str(kwargs['data_interval_start']))
         print('data_interval_end:'+str(kwargs['data_interval_end']))
         print('task_instance:'+str(kwargs['ti']))
+        return ''
+    
+    @task()
+    def python_function3(name:str):
+        print(f"hello {name}")        
+        return 
 
-    python_function1 >> python_function2()
+    python_t1 = PythonOperator(
+        task_id="python_t1",
+        python_callable=python_function1,
+        op_kwargs={'start_date':"{{data_interval_start | ds}}", "end_date":"{{data_interval_end | ds}}"}
+    )
+
+    # python_function1 >> python_function3('jhknag')    # python_function1 
+    # python_function3('jhknag') >>   python_t1  # python_function1 
+    # python_t1 >> python_function3('jhknag') >> python_function2() 
+    python_t1 >> python_function2() 
