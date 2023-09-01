3d949aa52cfa4ce142c5091ac352be82dccaee73
nuri428 <greennuri@gmail.com>
2023-09-01 16:17:27 +0900

    exmpale 수정 재업로드

--- dags/dags_python_templates.py
+++ dags/dags_python_templates.py
@@ -11,7 +11,7 @@ from airflow.decorators import task
 # catchup : 누락된 작업 내용을 한꺼번에 돌림. 
 
 with DAG(
-    dag_id="dags_python_show_templates",
+    dag_id="dags_python_templates",
     schedule="0 0 * * *",#crontab 
     start_date=pendulum.datetime(2021, 8, 31, tz="Asia/Seoul"),
     catchup=True,
