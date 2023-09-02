5ae25a80029221a20a0bf3e64e06b3bc5699d5f0
nuri428 <greennuri@gmail.com>
2023-09-02 17:21:31 +0900

    example name change

--- dags/dags_python_templates3.py
+++ dags/dags_python_templates3.py
@@ -13,7 +13,7 @@ from sqlalchemy import text, create_engine
 import sqlalchemy
 
 with DAG(
-    dag_id="dags_python_templates2",
+    dag_id="dags_python_templates3",
     schedule="0 0 * * *",#crontab
     start_date=pendulum.datetime(2021, 8, 31, tz="Asia/Seoul"),
     catchup=False,
